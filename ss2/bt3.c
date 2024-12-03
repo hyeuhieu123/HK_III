32e6f34ffa187985e36dd6d980c3d07aa14439f9
hieu <nguyentrunghieuthptxp@gmail.com>
2024-12-03 09:17:39 +0700

    add

--- ss2/bt3.c
+++ ss2/bt3.c
@@ -1,25 +1,18 @@
 #include <stdio.h>
-
-int main() {
-    int n;
-   
-    scanf("%d", &n);
-    int arr[n];
-    for (int i = 0; i < n; i++) {  
-        scanf("%d", &arr[i]);
-    };
-   for(int i =0;i<n-1;i++){
-   	for(int j=0;j<n-1-i;j++){
-   	if(arr[j]>arr[j+1]){
-   			int temp =arr[j];
-        arr[j] =arr[j + 1];
-        arr[j + 1] =temp;
-	   };
-	   };
-   };
-    
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+int main(){
+    int n = 6;
+    int start = 0;
+    int end = n - 1;
+    int arr[6] = {1,2,3,4,5,6};
+    while (start < end)
+    {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
     }
-    return 0;
+    for(int i = 0; i < n; i++){
+        printf("%d",arr[i]);
+    }
 }
